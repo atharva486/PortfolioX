8e5b5e2a5c2ee86100f7251bb92087ebc5aea05f
Atharva <atharva007ag@gmail.com>
2026-08-28 20:25:36 +0530

    updated response model in the route get'account'

--- app/api/routes/accounts.py
+++ app/api/routes/accounts.py
@@ -2,23 +2,19 @@ from fastapi import APIRouter,Depends,HTTPException
 from app.db.session import get_db
 from sqlalchemy.orm import Session
 from sqlalchemy import text
-from app.schemas.account_schema import createAccount
+from app.schemas.account_schema import CreateAccountRequest,AccountResponse
 from app.repositories.account_repository import AccountRepository
 
 router = APIRouter()
 
-# @app.get('/accounts/{account_id}')
-# def get_account():
-
-
 @router.post("/accounts")
-def create_account(account:createAccount, db:Session=Depends(get_db)):
+def create_account(account:CreateAccountRequest, db:Session=Depends(get_db)):
     data_db = account.model_dump()
     account1 = AccountRepository(session=db)
     new_account = account1.create_account(balance = data_db["balance"])
     return new_account
 
-@router.get('/accounts/{account_id}')
+@router.get('/accounts/{account_id}',response_model=AccountResponse)
 def get_account(account_id:int,db:Session = Depends(get_db)):
     account1 = AccountRepository(session=db)
     account = account1.get_account(account_id=account_id)
--- app/schemas/account_schema.py
+++ app/schemas/account_schema.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel,Field
 from decimal import Decimal
 
-class createAccount(BaseModel):
+class CreateAccountRequest(BaseModel):
     balance:Decimal=Field(gt=0,description="Initial balance must be positive")
 
-class AcccountResponse(BaseModel):
+class AccountResponse(BaseModel):
     id:int
     balance:Decimal
-    
+    model_config={"from_attributes":True}
+
 
